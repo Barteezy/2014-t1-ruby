2de46ca977cefd2ceacd1add58432914c4f8fdd0
Luke Bartel <bartelluke@gmail.com>
2014-02-11 23:35:44 -0700

    Homework

--- 207-colorful-vehicles/db/schema.rb
+++ 207-colorful-vehicles/db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130726035340) do
+ActiveRecord::Schema.define(version: 20140211214628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 
   create_table "colors", force: true do |t|
     t.string "name"
